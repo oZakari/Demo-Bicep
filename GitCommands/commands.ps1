0ddaa4de26758402f3e5c77eacdc46fe9c1c4ef0
Zach Trocinski <ztrocinski@microsoft.com>
2021-10-22 15:34:41 -0500

    added hello world

--- GitCommands/commands.ps1
+++ GitCommands/commands.ps1
@@ -15,4 +15,16 @@ git push
 
 git push --set-upstream main dev
 
+#Merge all commits
+git switch main 
+git pull
+git merge dev
+git push
+git switch dev
 
+#Cherry Pick Single commit
+git switch main 
+git pull
+git cherry-pick e1f740fcf161891a8f6b1503fe13a5f07248ae9e
+git push
+git switch dev
